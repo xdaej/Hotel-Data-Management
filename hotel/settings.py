eb82e1fc2e7f32c504dddb3d1eff9ac538bc7e55
genius <alex.rsilva18@gmail.com>
2017-11-29 15:25:29 -0500

    second commit

--- hotel/settings.py
+++ hotel/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'hotel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'), ]
