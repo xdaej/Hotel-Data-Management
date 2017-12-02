8e2539a81773174b30a3db79422d09576af66f38
genius <alex.rsilva18@gmail.com>
2017-12-02 02:40:30 -0500

    urls

--- hotel/urls.py
+++ hotel/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
+
+from registration import urls as registration_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^registration/', include(registration_urls, namespace='registration')),
+    url(r'^$', TemplateView.as_view(template_name='layout.html'), name='home'),
 ]
