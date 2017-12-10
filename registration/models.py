b0a40e43765ca31f8672e4848c0c0ed157ae07f0
Genius <alex.rsilva18@gmail.com>
2017-12-09 22:07:05 -0500

    authentication 2

--- registration/models.py
+++ registration/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django import forms
+
+class User(forms.ModelForm):
+    username = models.CharField(max_length=250)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.CharField(max_length=250)
+    #phone_number = models.IntegerField(max_length=250)
+    #address = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
 
 
 
