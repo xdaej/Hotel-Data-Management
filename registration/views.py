daaf80dab23fe6fa9b981ec216dca5e3bcb7a1fc
Genius <alex.rsilva18@gmail.com>
2017-12-09 22:02:50 -0500

    authentication

--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
 
 def registration(request):
     return render(request, 'registration/registration.html')
 
 
+def register(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('registration/success.html')
