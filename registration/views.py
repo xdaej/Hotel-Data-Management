964c6c3ed2dfa81117978280b7b68caf7ec82743
Genius <alex.rsilva18@gmail.com>
2017-12-09 22:28:58 -0500

    authentication 3

--- registration/views.py
+++ registration/views.py
@@ -6,10 +6,12 @@ from .forms import UserForm
 def registration(request):
     return render(request, 'registration/registration.html')
 
-
 def register(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('registration/success.html')
+    else:
+        form = UserForm()
+    return render(request, 'registration/registration.html', {'form':form})
